e79f3171cc0f9e770fd312593cdb0c90db42b0da
Farman Khan <farmankhan0046@gmail.com>
2022-10-12 14:23:29 +0530

    Authentication Complete

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User
 from .forms import MyUserCreationForm, LoginForm
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
 def loginPage(request):
     page = 'login'
     form = LoginForm
-
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('login')
     data = {
         'page':page,
         'form':form,
@@ -24,6 +34,7 @@ def signupPage(request):
         form = MyUserCreationForm(request.POST)
         if form.is_valid:
             form.save()
+            messages.success(request,"Account Created Successfully!")
             return redirect('home')
     data ={
         'form':form,
@@ -32,4 +43,5 @@ def signupPage(request):
     return render(request, 'accounts/login_register.html',data)
 
 def logoutPage(request):
+    logout(request)
     return redirect('home')
