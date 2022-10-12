2aa95dc1f19fbf2ccb16ccc4a15dfd461de93105
Farman Khan <farmankhan0046@gmail.com>
2022-10-12 14:47:19 +0530

    Movie Poster Functionality

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class MoviePoster(models.Model):
+    GENRE_CHOICES = [
+        ('action', 'Action'),
+        ('comedy', 'Comedy'),
+        ('drama', 'Drama'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('thriller', 'Thriller'),
+        ('sports', 'Sports'),
+        ('adventure', 'Adventure'),
+    ]
+    title = models.CharField(max_length = 200, null=True, blank=True)
+    genre = models.CharField(max_length = 30, choices = GENRE_CHOICES, blank = True, null = True)
+    poster = models.ImageField(upload_to = 'media/%Y/%m/')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.title
